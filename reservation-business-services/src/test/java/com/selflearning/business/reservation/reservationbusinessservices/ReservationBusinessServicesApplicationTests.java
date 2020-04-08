6ff2bf5ca55e182f40dec3c39bc25cf33e443b93
Noura Mozaffari <noura.mozaffari@gmail.com>
2020-04-08 16:40:03 +0430

    reservation-business-services

---
+++ reservation-business-services/src/test/java/com/selflearning/business/reservation/reservationbusinessservices/ReservationBusinessServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selflearning.business.reservation.reservationbusinessservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationBusinessServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
